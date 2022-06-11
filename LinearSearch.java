2f6e654a96b37546268bd4037eb3a51297666993
Rajat Verma <104197270+Rajatvermalko@users.noreply.github.com>
2022-06-11 15:19:17 +0530

    feat add comments

--- LinearSearch.java
+++ LinearSearch.java
@@ -1,6 +1,5 @@
-import java.sql.SQLOutput;
+//To Find the number in the user defined array through linear search
 import java.util.Scanner;
-
 import static java.lang.System.in;
 
 public class LinearSearch
@@ -9,16 +8,16 @@ public class LinearSearch
     {
         Scanner sc=new Scanner(in);
         System.out.println("Enter the size of arrays and then the array ");
-        int[] arr=new int[sc.nextInt()];
+        int[] arr=new int[sc.nextInt()];            //initialization of user defined array
         for (int i=0;i<arr.length;i++)
-        arr[i]=sc.nextInt();
+        arr[i]=sc.nextInt();                       //taking inputs in the array
         System.out.println("Enter the number you need to find in the array: ");
-        int n = sc.nextInt();
+        int n = sc.nextInt();                      // inputing the number we want to find in the array
         int flag=0;
         for (int i=0;i<arr.length;i++)
         {
             if(arr[i]==n)
-                flag++;
+                flag++;                            //updating flag when number is found in the array
         }
         if (flag>0)
             System.out.println("Number "+n+" is "+flag+" times present in the array.");
