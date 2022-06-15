9690506dfc0bbd57791014f82f8946158ca60131
Rajat Verma <104197270+Rajatvermalko@users.noreply.github.com>
2022-06-15 17:57:10 +0530

    feat comments
    added comments for the better understanding of the program and logic

--- FloorBINARY.java
+++ FloorBINARY.java
@@ -1,20 +1,22 @@
+//WAP to find out the floor number in the array
+// take necessary inputs from user
 import java.util.Scanner;
 
 public class FloorBINARY
 {
-    public static void main(String[] args)
+    public static void main(String[] args)                          //main function
     {
         Scanner sc = new Scanner(System.in);
         System.out.print("First Enter the size of array and then ascending sorted elements in the array: ");
-        int[] arr = new int[sc.nextInt()];
+        int[] arr = new int[sc.nextInt()];                         //to input the array from user
         for (int i = 0; i < arr.length; i++)
-            arr[i] = sc.nextInt();
+            arr[i] = sc.nextInt();                                 //to input number to find in the array
         System.out.print("Enter the number you want to find out in the array: ");
-        System.out.println(FloorBINARY.calc(sc.nextInt(), arr)+ " - is the floor number");
+        System.out.println(FloorBINARY.calc(sc.nextInt(), arr)+ " - is the floor number");   //function calling and printing the answer
     }
-        public static int calc(int n,int[] a)
+        public static int calc(int n,int[] a)                      //calc function to calculate the floor number in the array
         {
-        int start = 0, end = a.length- 1, mid;
+        int start = 0, end = a.length- 1, mid;                    //calculations and logic to find out the floor number
         while(start<end)
         {
             mid=(start+(end-start))/2;
@@ -33,6 +35,6 @@ public class FloorBINARY
              end=mid-1;
             }
         }
-            return start;
+            return 0;
         }
 }
